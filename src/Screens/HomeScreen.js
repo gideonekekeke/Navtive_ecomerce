75aac2d182ebf94bda2f00127653b02f9e90091e
gideonekekeke <gideonekeke64@gmail.com>
2023-05-30 07:46:05 +0100

    first commit

---
+++ src/Screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet } from 'react-native/types'
+
+const HomeScreen = () => {
+  return (
+    <div style ={styles.container}>HomeScreen</div>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+  container : {
+    
+  }
+})
